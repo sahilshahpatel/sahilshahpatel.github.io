b97d9193a44320d7f7de5f089c22d90eccac390f
Sahil Patel <sahilshahpatel@gmail.com>
2024-03-21 21:10:50 -0700

    astro: Fix site config
    First attempt at fixing rogue personal page dialogs caused by
    Astro.url.hostname always returning localhost.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), mdx()]
+  integrations: [image(), mdx()],
+  site: "https://sahilshahpatel.com"
 });
