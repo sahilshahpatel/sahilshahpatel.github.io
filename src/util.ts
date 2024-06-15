8b8f246318fa9e5f52a37838645c460bfae73335
Sahil Patel <sahilshahpatel@gmail.com>
2024-06-15 16:38:15 -0700

    reviews: Move from remote images to local ones
    Google books images were getting the dog-eared corner thing, and
    I didn't like it, so I'm moving to just locally storing the images
    which is better in general.

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,6 @@
 import type { CollectionEntry } from 'astro:content';
 import type { ImageMetadata } from 'astro';
+import { imageMetadata } from 'astro/assets/utils';
 
 /***********************************/
 /* Astro Assets imports            */
@@ -17,15 +18,28 @@ const assetsManifest = {
         folder: "/src/assets/images/logos",
         images: import.meta.glob<{default: ImageMetadata }>("/src/assets/images/logos/*.{jpg,jpeg,png,gif,webp}"),
     },
+    "reviews": {
+        folder: "/src/assets/images/reviews",
+        images: import.meta.glob<{default: ImageMetadata}>("/src/assets/images/reviews/*.{jpg,jpeg,png,gif,webp}"),
+    },
     "collections": {
         folder: "/src/assets/images/collections",
         images: import.meta.glob<{default: ImageMetadata }>("/src/assets/images/collections/*.{jpg,jpeg,png,gif,webp}"),
     }
 }
 
-export async function getImage(collection: keyof typeof assetsManifest, basename: string) {
+export async function getImage(collection: keyof typeof assetsManifest, basename: string, findExtension: boolean = false) {
     const manifest = assetsManifest[collection];
-    const path = `${manifest.folder}/${basename}`;
+    let path = `${manifest.folder}/${basename}`;
+
+    if (findExtension) {
+        Object.keys(manifest.images).forEach(key => {
+            if (key.startsWith(path)) {
+                path = key;
+            }
+        })
+    }
+
     const imgFunc = manifest.images[path];
     if (!imgFunc) throw Error(`Unable to find image at ${path}`);
     const img = await imgFunc();
@@ -48,40 +62,4 @@ interface DatedCollectionItem {
 }
 export function dateSort<T extends DatedCollectionItem>(list: T[]) {
     return list.sort((a, b) => a.data.date.getTime() - b.data.date.getTime());
-}
-
-
-/***********************************/
-/* Review item thumbnail functions */
-/***********************************/
-
-export async function getReviewItemThumbnail(item: CollectionEntry<"reviews">["data"]): Promise<string> {
-    // TODO: Implement functions for all cases
-    switch (item.type){
-        case "book":        return getImgFromIsbn(item.isbn!);
-        case "video game":  return getImgFromSteamAppId(item.steamAppId!);
-        default:
-            console.log(`WARNING: Unable to get thumbnail for media type ${item.type} (for ${item.title})`)
-            return "";
-    }
-}
-
-async function getImgFromIsbn(isbn: string): Promise<string> {
-    const googleBooksUrl = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`
-    const _response = await fetch(googleBooksUrl)
-    const response = await _response.json()
-
-    if (response.items.length != 1) {
-        throw Error("Google Books isbn queries should only return one result!")
-    }
-    if (response.items[0].volumeInfo.imageLinks == undefined){
-        throw Error("Google Books query returned no images!")
-    }
-
-    return response.items[0].volumeInfo.imageLinks.thumbnail;
-}
-
-function getImgFromSteamAppId(id: string): string {
-    // TODO: This is a hack which may not work for all games
-    return `https://steamcdn-a.akamaihd.net/steam/apps/${id}/library_600x900_2x.jpg`
 }
